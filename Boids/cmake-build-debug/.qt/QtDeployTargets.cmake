d3b9cfa71c52bbaf831171b0e493dfbf6e6449c5
Green Kiera <s5610456@pg10519.student.bournemouth.ac.uk>
2025-05-25 14:53:48 +0100

    update

--- Boids/cmake-build-debug/.qt/QtDeployTargets.cmake
+++ Boids/cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -1,1 +1,1 @@
-set(__QT_DEPLOY_TARGET_Boids_FILE /home/s5610456/CFGAA/programming-assignment-5610456/flock/Boids/cmake-build-debug/Boids)
+set(__QT_DEPLOY_TARGET_Boids_FILE /home/s5610456/CFGAA/programming-assignment-5610456/flock/programming-assignment-5610456/Boids/cmake-build-debug/Boids)
--- Boids/cmake-build-debug/cmake_install.cmake
+++ Boids/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/s5610456/CFGAA/programming-assignment-5610456/flock/Boids
+# Install script for directory: /home/s5610456/CFGAA/programming-assignment-5610456/flock/programming-assignment-5610456/Boids
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/s5610456/CFGAA/programming-assignment-5610456/flock/Boids/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/s5610456/CFGAA/programming-assignment-5610456/flock/programming-assignment-5610456/Boids/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
